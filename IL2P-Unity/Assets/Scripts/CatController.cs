7560a47c9088da1f15a715ce2f8dda936008fd45
Sam <sweatersjpg@gmail.com>
2021-02-27 21:46:43 -0500

    Added Cat, cat animations, launching mechanic, controller
    I have forgotten to push all day!

---
+++ IL2P-Unity/Assets/Scripts/CatController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sr;
+
+    bool airborn;
+    bool stunned;
+
+    float pVely;
+
+    Transform arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        arrow = transform.Find("arrow");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 dir = mousePosition - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        arrow.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        arrow.gameObject.SetActive(!airborn && rb.velocity.x==0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.y == 0 && pVely == 0)
+        {
+            airborn = false;
+            stunned = false;
+        }
+        else
+        {
+            airborn = true;
+        }
+
+        if (rb.velocity.x < 0) sr.flipX = true;
+        if (rb.velocity.x > 0) sr.flipX = false;
+
+        anim.SetBool("Airborn", airborn);
+        anim.SetBool("Stunned", stunned);
+        anim.SetFloat("YVelocity", rb.velocity.y);
+
+        pVely = rb.velocity.y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (airborn)
+        {
+            stunned = true;
+        }
+    }
+
+    private void Launch(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+}
---
+++ IL2P-Unity/Assets/Scripts/Controller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject cat; // for reading cat object
+    GameObject arrow;
+
+    [SerializeField]
+    float maxPower; // max power
+    [SerializeField]
+    float minPower; // min power
+    [SerializeField]
+    float lerpDuration; // power bar fill duration
+
+    float power;
+    float timeElapsed;
+
+    [SerializeField]
+    AnimationCurve powerCurve;
+
+    [SerializeField]
+    Slider powerBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0;
+        powerBar.minValue = minPower;
+        powerBar.maxValue = maxPower;
+
+        arrow = cat.transform.Find("arrow").gameObject; // get arrow child to check if it's active (instead of using sendMessage cause I'm lazy)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // get mouse position in world space
+
+        if (arrow.activeSelf) // if arrow is shown (hack)
+        {
+
+            if (Input.GetMouseButton(0)) // when mouse is held
+            {
+                timeElapsed += Time.deltaTime; // inscrease timeElapsed
+                power = Mathf.Lerp(minPower, maxPower, powerCurve.Evaluate(timeElapsed / lerpDuration)); // lerp power with AnimationCurve
+                powerBar.value = power; // update powerBar
+            }
+
+            if (Input.GetMouseButtonUp(0)) // when mouse is released
+            {
+                Vector2 direction = mousePosition - cat.transform.position; // get direction from cat to mouse position 
+                direction.Normalize(); // normalize it so it's a real direction vector
+                direction *= power; // set it's magnitude to power level
+
+                cat.SendMessage("Launch", direction); // send force to cat
+
+                power = minPower;
+                timeElapsed = 0;
+                powerBar.value = power;
+            }
+        }
+
+    }
+}
